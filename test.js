4daa1699c29ce07320e8ad7f1d17ff27ea03f355
Bohdan <bohdan.khamidulin@gmail.com>
2023-01-05 18:44:17 +0200

    2222

--- test.js
+++ test.js
@@ -1,4 +1,17 @@
 /** @format */
 
-const name = "papa";
-console.log("name");
+const age = 20;
+const type = age >= 18 ? "adult" : "child";
+console.log(type);
+
+const num1 = 5;
+const num2 = 10;
+let biggerNumber;
+
+if (num1 > num2) {
+  biggerNumber = num1;
+} else {
+  biggerNumber = num2;
+}
+
+console.log(5);
